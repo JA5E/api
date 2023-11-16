ebe6915588d84cbf4b42e0ef73eaa9b5b9ffc625
JA5E <jase0485@gmail.com>
2023-11-15 20:04:53 -0600

    view updated for get methods

--- index.php
+++ index.php
@@ -24,27 +24,90 @@
     </tbody>
   </table>
 
-  <script>
-    // Realizar la solicitud GET a la API en PHP
-    fetch('method.php')
+  <br><br>
+  <div id="averageText">
+  </div>
+  <br><br>
+
+
+  <table>
+    <thead>
+      <tr>
+        <th>Repeted data</th>
+        <th>count</th>
+      </tr>
+    </thead>
+    <tbody id="countT">
+      <!-- Los resultados de la API se mostrarán aquí -->
+    </tbody>
+  </table>
+  <br><br>
+
+<!-- REQUESTS FOR THE DIFERENT DATA WE NEED -->
+    <script>
+      // Realizar la solicitud GET a la API en PHP
+      fetch('method.php')
+        .then(response => response.json())
+        .then(data => {
+          const tablaBody = document.getElementById('tabla-body');
+          data.slice(0, 10).forEach(item => { // Limit to 10 rows
+            const row = document.createElement('tr');
+            row.innerHTML = `
+              <td>${item.id}</td>
+              <td>${item.desc_municipio}</td>
+              <td>${item.indicador}</td>
+              <td>${item.ano}</td>
+              <td>${item.valor}</td>
+              <td>${item.unidad_medida}</td>
+            `;
+            tablaBody.appendChild(row);
+          });
+        })
+        .catch(error => console.error('Error:', error));
+    </script>
+    <script>
+      fetch('method.php?average=1')
       .then(response => response.json())
       .then(data => {
-        const tablaBody = document.getElementById('tabla-body');
-        data.slice(0, 10).forEach(item => { // Limit to 10 rows
-          const row = document.createElement('tr');
-          row.innerHTML = `
-            <td>${item.id}</td>
-            <td>${item.desc_municipio}</td>
-            <td>${item.indicador}</td>
-            <td>${item.ano}</td>
-            <td>${item.valor}</td>
-            <td>${item.unidad_medida}</td>
-          `;
-          tablaBody.appendChild(row);
-        });
+
+          const text = document.getElementById('averageText');
+          text.innerHTML += `
+                            <h2>Average of valor column: ${data[0].average}</h2>
+                        `;
       })
       .catch(error => console.error('Error:', error));
-  </script>
+    </script>
+    <script>
+        fetch('method.php?repeted=1')
+          .then(response => response.json())
+          .then(data => {
+            const countBody = document.getElementById('countT');
+            data.forEach(item => { 
+              console.log(item);
+              const row = document.createElement('tr');
+              row.innerHTML = `
+                <td>${item.repeated}</td>
+                <td>${item.counts}</td>
+              `;
+              countBody.appendChild(row);
+            });
+          })
+          .catch(error => console.error('Error:', error));
+    </script>
+    <script>
+      fetch('method.php?media=1')
+      .then(response => response.json())
+      .then(data => {
+
+          const text = document.getElementById('averageText');
+          text.innerHTML += `
+                            <h2>Median of valor column: ${data[0].Median}</h2>
+                        `;
+      })
+      .catch(error => console.error('Error:', error));
+    </script>
+
+
+
 </body>
 </html>
-
--- method.php
+++ method.php
@@ -11,7 +11,8 @@ define("DB_NAME", "mortalidad");
 // Usuario de la base de datos
 define("DB_USERNAME", "root");
 // Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "root");
+
 ///////////////////////////////////////////////////////////////////////////
 
 // Conexión a la database utilizando mysqli
@@ -30,14 +31,23 @@ switch ($request_method) {
 	
 
 		if (isset($_GET['average'])) {
-			$sql = "SELECT AVG(valor) FROM zacatecas WHERE unidad_medida = 'Defunciones registradas';";
+			$sql = "SELECT AVG(valor) as average FROM zacatecas;";
 		}elseif (isset($_GET['repeted'])){
-			$sql = "SELECT indicador, COUNT(*) AS repeted_values FROM zacatecas GROUP BY 1; ";
+			$sql = "SELECT indicador as repeated , COUNT(*) AS counts FROM zacatecas GROUP BY 1;";
 		}elseif (isset($_GET['media'])){
 		#SELECT AVG(valor) AS mediana_valor FROM ( SELECT valor, @rownum:=@rownum+1 as `row_number`, @total_rows:=@rownum FROM (SELECT valor FROM zacatecas ORDER BY valor) as tu_tabla_ordenada, (SELECT @rownum:=0) r ) as ordered WHERE `row_number` IN (FLOOR((@total_rows+1)/2), FLOOR((@total_rows+2)/2)); 
-			$sql = "SELECT AVG(valor) AS median_valor FROM ( SELECT valor, @rownum := @rownum + 1 AS row_number, @total_rows := @rownum FROM zacatecas JOIN (SELECT @rownum := 0) r ORDER BY valor ) AS ordered WHERE row_number IN (FLOOR((@total_rows + 1) / 2), FLOOR((@total_rows + 2) / 2)); ";
+			$sql = "SELECT
+						AVG(d.valor) as Median 
+					FROM
+						(SELECT @rowindex:=@rowindex + 1 AS rowindex,
+								zacatecas.valor AS valor
+						FROM zacatecas
+						ORDER BY zacatecas.valor) AS d
+					WHERE
+					d.rowindex IN (FLOOR(@rowindex / 2), CEIL(@rowindex / 2)); 
+					";
+					$conexion->query("SET @rowindex := -1;");
 		}else{
-			#echo "No parameter received.";
 			// Consulta SQL para seleccionar todos los registros de la tabla "blogs"
 			$sql = "SELECT id, cve_municipio, desc_municipio, id_indicador, indicador, ano, valor, unidad_medida FROM zacatecas;";
 			// Ejecutar la consulta
